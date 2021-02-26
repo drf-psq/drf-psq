875ab9731555a3bfc57dd3af7ceb4e15c54b6526
AminHP <mdan.hagh@gmail.com>
2021-02-26 21:21:04 +0330

    Release v1.1.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 description = (
     "The simplest and most general way to manage action-based "
-    "permissions, serializers, and querysets dependent on permission-based"
+    "permissions, serializers, and querysets dependent on permission-based "
     "rules for the Django REST framework!"
 )
 
 setup(
     name='drf_psq',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
 
     description=description,
